38c3afc725f29d1242620f1dc624c76264370fd7
Johnjoe Landers <johnjoelanders@outlook.com>
2016-11-30 22:26:46 +0000

    Created an onclick listener for each button and will display a toast with a bit of text

--- app/src/main/java/com/example/john/ocrapplication/MainActivity.java
+++ app/src/main/java/com/example/john/ocrapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.john.ocrapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCamera = (Button)findViewById(R.id.btnOpenCamera);
+        Button btnGallery = (Button)findViewById(R.id.btnGallery);
+        Button btnConvert = (Button)findViewById(R.id.btnConvert);
+
+        if (btnCamera != null) {
+            btnCamera.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "You opened the camera";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            });
+        }
+
+        if (btnGallery != null) {
+            btnGallery.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "You opened the gallery";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                }
+            });
+        }
+
+        if (btnConvert != null) {
+            btnConvert.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "You clicked convert";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                }
+            });
+        }
     }
 }
