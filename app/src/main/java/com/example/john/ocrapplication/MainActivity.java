5f7139cff468e388210fbcee7dbfffb8d3025d19
Johnjoe Landers <johnjoelanders@outlook.com>
2017-02-07 22:06:48 +0000

    Removed unused imports. When the button gallery is clicked it now adds the picture to the image view. To stop the camera from crashing i addded in a switch statement which checks which request code it is.

--- app/src/main/java/com/example/john/ocrapplication/MainActivity.java
+++ app/src/main/java/com/example/john/ocrapplication/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.john.ocrapplication;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,19 +16,23 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
-    Button btnCamera,btnGallery,btnConvert;
-    static final  int CAM_REQUEST = 1;
+    Button btnCamera, btnGallery, btnConvert;
+    static final int CAM_REQUEST = 1;
+    static final int GALLERY_REQUEST = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView =(ImageView)findViewById(R.id.imageView);
-        btnCamera = (Button)findViewById(R.id.btnOpenCamera);
-        btnGallery = (Button)findViewById(R.id.btnGallery);
-        btnConvert = (Button)findViewById(R.id.btnConvert);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        btnCamera = (Button) findViewById(R.id.btnOpenCamera);
+        btnGallery = (Button) findViewById(R.id.btnGallery);
+        btnConvert = (Button) findViewById(R.id.btnConvert);
 
         if (btnCamera != null) {
             btnCamera.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     File file = getFile();
                     camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-                    startActivityForResult(camera_intent,CAM_REQUEST);
+                    startActivityForResult(camera_intent, CAM_REQUEST);
                 }
             });
         }
@@ -41,13 +48,14 @@ public class MainActivity extends AppCompatActivity {
         if (btnGallery != null) {
             btnGallery.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    Context context = getApplicationContext();
-                    CharSequence text = "You opened the gallery";
-                    int duration = Toast.LENGTH_SHORT;
+                    Intent photoFromGallery = new Intent(Intent.ACTION_PICK);
 
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
+                    File picturePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                    String picturePathAsString = picturePath.getPath();
+                    Uri data = Uri.parse(picturePathAsString);
 
+                    photoFromGallery.setDataAndType(data, "image/*");
+                    startActivityForResult(photoFromGallery, GALLERY_REQUEST);
                 }
             });
         }
@@ -66,18 +74,36 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
-    private File getFile(){
+
+    private File getFile() {
         File folder = new File("sdcard/camera_app");
-        if(!folder.exists()){
+        if (!folder.exists()) {
             folder.mkdir();
         }
-        File image_file = new File(folder,"cam_image.jpg");
+        File image_file = new File(folder, "cam_image.jpg");
         return image_file;
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        String path = "sdcard/camera_app/cam_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case GALLERY_REQUEST:
+                Uri imageUri = data.getData();
+                InputStream inputStream;
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+                    imageView.setImageBitmap(image);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Cannot open image", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case CAM_REQUEST:
+                String path = "sdcard/camera_app/cam_image.jpg";
+                imageView.setImageDrawable(Drawable.createFromPath(path));
+                break;
+        }
 
     }
 
