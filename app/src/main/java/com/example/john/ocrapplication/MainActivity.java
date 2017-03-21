3d245d3b07b0cfe2d6c387a3a12af1aef8197e69
johnjoelanders <johnjoelanders@outlook.com>
2017-03-21 13:41:34 +0000

    Removed old code in order to get tesseract workking added 6 methods in doOCR it calls the prepare tesseract method and calls the startOcr method and gives it the image bitmap and converts to black and white. the prepareDirectory method  which creates the a directory in the sdcard tesseractsample/tessdata the moveFiles moves the training data to the tessdata folder. the extract text from bitmap gets the charaters from the bitmap using the tesseract api

--- app/src/main/java/com/example/john/ocrapplication/MainActivity.java
+++ app/src/main/java/com/example/john/ocrapplication/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.john.ocrapplication;
 
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,26 +8,43 @@ import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class MainActivity extends AppCompatActivity{
+
     ImageView imageView;
     Button btnCamera, btnGallery, btnConvert;
-    static final int CAM_REQUEST = 1;
-    static final int GALLERY_REQUEST = 2;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    Uri outputFileUri;
+    private static final String lang = "eng";
+    String extractedText;
+    Bitmap imageBitmap;
+    private TessBaseAPI tessBaseApi;
+    TextView textView;
+    static final int CONVERT = 1;
+
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/TesseractSample/";
+    private static final String TESSDATA = "tessdata";
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,94 +54,188 @@ public class MainActivity extends AppCompatActivity {
         btnCamera = (Button) findViewById(R.id.btnOpenCamera);
         btnGallery = (Button) findViewById(R.id.btnGallery);
         btnConvert = (Button) findViewById(R.id.btnConvert);
-
-        if (btnCamera != null) {
-            btnCamera.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File file = getFile();
-                    camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-                    startActivityForResult(camera_intent, CAM_REQUEST);
-                }
-            });
-        }
-
-        if (btnGallery != null) {
-            btnGallery.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent photoFromGallery = new Intent(Intent.ACTION_PICK);
-
-                    File picturePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-                    String picturePathAsString = picturePath.getPath();
-                    Uri data = Uri.parse(picturePathAsString);
-
-                    photoFromGallery.setDataAndType(data, "image/*");
-                    startActivityForResult(photoFromGallery, GALLERY_REQUEST);
-                }
-            });
-        }
+        textView = (TextView) findViewById(R.id.textResult);
 
         if (btnConvert != null) {
             btnConvert.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    Context context = getApplicationContext();
-                    CharSequence text = "You clicked convert";
-                    int duration = Toast.LENGTH_SHORT;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
+                        startCameraActivity();
 
                 }
             });
         }
     }
+    private void startCameraActivity(){
+        try {
+            String IMGS_PATH = Environment.getExternalStorageDirectory().toString() + "/TesseractSample/imgs";
+            prepareDirectory(IMGS_PATH);
 
-    private File getFile() {
-        File folder = new File("sdcard/camera_app");
-        if (!folder.exists()) {
-            folder.mkdir();
+            String img_path = IMGS_PATH + "/ocr.jpg";
+
+            outputFileUri = Uri.fromFile(new File(img_path));
+
+            final Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, CONVERT);
+                imageBitmap = BitmapFactory.decodeFile(img_path);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
         }
-        File image_file = new File(folder, "cam_image.jpg");
-        return image_file;
+    }
+    public void onActivityResult(int requestCode, int resultCode,Intent data) {
+        if (requestCode == CONVERT) {
+            doOCR();
+        } else {
+            Toast.makeText(this, "ERROR: Image was not obtained.", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode){
-            case GALLERY_REQUEST:
-                Uri imageUri = data.getData();
-                InputStream inputStream;
-                try {
-                    inputStream = getContentResolver().openInputStream(imageUri);
-                    Bitmap image = BitmapFactory.decodeStream(inputStream);
-                    imageView.setImageBitmap(convertImageToGreyScale(image));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "Cannot open image", Toast.LENGTH_SHORT).show();
+    private void doOCR() {
+        prepareTesseract();
+        startOCR(convertColorIntoBlackAndWhiteImage(imageBitmap));
+    }
+
+    private void prepareDirectory(String path) {
+
+        File dir = new File(path);
+        Log.i(TAG,"This is the path"+path);
+        if (!dir.exists()) {
+            if (!dir.mkdirs()) {
+                Log.e(TAG, "ERROR: Creation of directory " + path);
+            }
+        } else {
+            Log.i(TAG, "Created directory " + path);
+        }
+    }
+
+
+    private void prepareTesseract() {
+        try {
+            prepareDirectory(Environment.getExternalStorageDirectory().toString() + "/TesseractSample/tessdata");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        moveFiles(TESSDATA);
+    }
+
+    private void moveFiles(String path) {
+        try {
+            String fileList[] = getAssets().list(path);
+
+            for (String fileName : fileList) {
+
+                String pathToDataFile = DATA_PATH + path + "/" + fileName;
+                if (!(new File(pathToDataFile)).exists()) {
+
+                    InputStream in = getAssets().open(path + "/" + fileName);
+
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+
+                    byte[] buf = new byte[1024];
+                    int len;
+
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                    in.close();
+                    out.close();
+
+                    Log.d(TAG, "Copied " + fileName + "to tessdata");
                 }
-                break;
-            case CAM_REQUEST:
-                String path = "sdcard/camera_app/cam_image.jpg";
-                imageView.setImageDrawable(Drawable.createFromPath(path));
-                break;
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to copy files to tessdata " + e.toString());
         }
-
     }
-    //Convert to greyscale
-    public Bitmap convertImageToGreyScale(Bitmap imageBitmap){
-        int widthOfImage,heightOfImage;
-        widthOfImage = imageBitmap.getWidth();
-        heightOfImage = imageBitmap.getHeight();
 
-        Bitmap bitmapGreyScale = Bitmap.createBitmap(widthOfImage,heightOfImage,Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmapGreyScale);
-        Paint paint = new Paint();
+    private String extractTextFromBitmap(Bitmap bitmap) {
+        try{
+            tessBaseApi = new TessBaseAPI();
+        }catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+        tessBaseApi.init(DATA_PATH,lang);
+        tessBaseApi.setImage(bitmap);
+        try {
+            extractedText = tessBaseApi.getUTF8Text();
+        } catch (Exception e) {
+            Log.e(TAG, "Error in recognizing text.");
+        }
+        tessBaseApi.end();
+        return extractedText;
+    }
+    private void startOCR(final Bitmap bitmap) {
+        if (mProgressDialog == null) {
+            mProgressDialog = ProgressDialog.show(this, "Processing",
+                    "Please wait...", true);
+        }
+        else {
+            mProgressDialog.show();
+        }
+        new Thread(new Runnable() {
+            public void run() {
+
+                final String result = extractTextFromBitmap(bitmap).toLowerCase();
+
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        if (result != null && !result.equals("")) {
+                            String s = result.trim();
+                            textView.setText(result);
+
+                            imageView.setImageBitmap(bitmap);
+
+                            Log.i(TAG, "S is "+s);
+
+                        }
+
+                        mProgressDialog.dismiss();
+                    }
+
+                });
+
+            };
+        }).start();
+    }
+    private Bitmap convertColorIntoBlackAndWhiteImage(Bitmap orginalBitmap) {
         ColorMatrix colorMatrix = new ColorMatrix();
         colorMatrix.setSaturation(0);
-        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
-        paint.setColorFilter(filter);
-        canvas.drawBitmap(imageBitmap,0,0,paint);
-        return bitmapGreyScale;
+
+        ColorMatrixColorFilter colorMatrixFilter = new ColorMatrixColorFilter(
+                colorMatrix);
+
+        Bitmap blackAndWhiteBitmap = orginalBitmap.copy(
+                Bitmap.Config.ARGB_8888, true);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(colorMatrixFilter);
+
+        Canvas canvas = new Canvas(blackAndWhiteBitmap);
+        canvas.drawBitmap(blackAndWhiteBitmap, 0, 0, paint);
+
+        return blackAndWhiteBitmap;
     }
+//     class OcrOperationAsync extends AsyncTask<Void,Void,String>{
+//
+//         @Override
+//         protected String doInBackground(Void... params) {
+//             try{
+//                 BitmapFactory.Options options = new BitmapFactory.Options();
+//                 options.inSampleSize = 4;
+//                 Bitmap bitmap = BitmapFactory.decodeFile(outputFileUri.getPath(), options);
+//                 result = extractText(bitmap);
+//             }catch(Exception e){
+//                 Log.e(TAG,"The error is "+e);
+//             }
+//             return result;
+//         }
+//     }
 
 }
