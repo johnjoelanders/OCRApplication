a30bb32ba7d77f247ceff29cadb6fb0b79be9be3
johnjoelanders <johnjoelanders@outlook.com>
2017-05-03 12:16:35 +0100

    Removed commented out code

--- app/src/main/java/com/example/john/ocrapplication/ImageProcessing.java
+++ app/src/main/java/com/example/john/ocrapplication/ImageProcessing.java
@@ -1,33 +1,10 @@
 package com.example.john.ocrapplication;
 
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
-import android.os.Environment;
 
-import java.io.File;
+class ImageProcessing {
 
-public class ImageProcessing {
-    int widthOfImage, heightOfImage;
-    //do binirization
-//    public Bitmap BinarizeImage(Bitmap imageBitmap) {
-//        widthOfImage = imageBitmap.getWidth();
-//        heightOfImage = imageBitmap.getHeight();
-//
-//        Bitmap bitmapGreyScale = Bitmap.createBitmap(widthOfImage, heightOfImage, Bitmap.Config.ARGB_8888);
-//        Canvas canvas = new Canvas(bitmapGreyScale);
-//        Paint paint = new Paint();
-//        ColorMatrix colorMatrix = new ColorMatrix();
-//        colorMatrix.setSaturation(0);
-//        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
-//        paint.setColorFilter(filter);
-//        canvas.drawBitmap(imageBitmap, 0, 0, paint);
-//
-//        return bitmapGreyScale;
-//    }
 
     public Bitmap findContours(Bitmap bitmap){
 
@@ -67,12 +44,4 @@ public class ImageProcessing {
         }
         return bitmap;
     }
-    //Inverse the color
-    //erosion
-    //Seperate contours
-    //Find all contours
-    //Delete all contours that width more than height  or do edge detection
-    // find large rectangle that contains remain contours
-    //http://stackoverflow.com/questions/33881175/remove-background-noise-from-image-to-make-text-more-clear-for-ocr
-    //https://nayefreza.wordpress.com/2013/05/15/outer-contour-tracing-using-square-tracing-algorithm-for-binary-image-java-implementation/
 }
